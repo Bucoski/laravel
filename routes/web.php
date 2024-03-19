62ff1ea73e41e6975103988761b456c0153c652b
Wagner Bucoski <bucoski.wagner@gmail.com>
2024-03-19 16:30:38 -0300

    first tests

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/empresa', function () {
+    return view('site/empresa');
+});
+
+Route::get('/produto/{id}/{nome}', function($id, $nome){
+    return "O id do produto é: " . $id . "<br>O nome é: " . $nome;
+});
